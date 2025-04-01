63723957c1bd86cf082b154e22d22fafaed5c859
Vincent Ostrowski <vincentostrovski@gmail.com>
2025-04-01 19:39:34 -0400

    Load insights for selected book and manage insight tree and map state before Book Screen

--- client/components/library/BookItem.tsx
+++ client/components/library/BookItem.tsx
@@ -1,7 +1,8 @@
 import { Text, Image, StyleSheet, Dimensions, Pressable } from 'react-native';
 import { router } from 'expo-router';
-import type { Book } from "../../data/types";
+import type { Book, Insight } from "../../data/types";
 import { useBook } from "../../data/bookContext";
+import { supabase } from "../../config/supabase";
 
 type Props = {
   book: Book;
@@ -13,14 +14,50 @@ const COLUMN_COUNT = 3;
 const PADDING = 8;
 const itemWidth = (width - (PADDING * 2 * COLUMN_COUNT)) / COLUMN_COUNT;
 
+function buildInsightTreeAndMap(allInsights: Insight[]) {
+    const map: Record<string, Insight> = {};
+    const tree: Insight[] = [];
+  
+    allInsights.forEach((insight) => {
+      map[insight.id] = { ...insight, children: [] };
+    });
+  
+    allInsights.forEach((insight) => {
+      if (insight.parentId) {
+        map[insight.parentId]?.children.push(map[insight.id]);
+      } else {
+        tree.push(map[insight.id]);
+      }
+    });
+    return {tree, map};
+  }
+
 export function BookItem({ book }: Props) {
-  const { setSelectedBook } = useBook();
+  const { selectedBook, setSelectedBook, setInsightTree, setInsightMap } = useBook();
+
+  const loadAllInsights = async () => {
+    setInsightTree([]);
+    setInsightMap({});
+    let {data: allInsights, error} = await supabase.from('book_insights_with_saved_state').select('*').eq('bookId', book.id);
+    if (allInsights && allInsights.length > 0) {
+      const { tree, map } = buildInsightTreeAndMap(allInsights);
+      setInsightTree(tree);
+      setInsightMap(map);
+    } 
+    if (error) {
+      console.error('Error fetching insights:', error.message);
+    }
+  };
 
   return (
     <Pressable 
       style={styles.container}
-      onPress={() => {
-        setSelectedBook(book);
+      onPress={async () => {
+        if (!selectedBook || selectedBook.id != book.id) {
+          await loadAllInsights();
+          setSelectedBook(book);
+        }
+
         router.push({
           pathname: "/library/[book]",  
           params: {book: book.id }
